3cc8681a93238719ad77fe38fc0c702417b932ae
Mo Zhou <weekenthralling@gmial.com>
2024-07-18 14:12:33 +0800

    feat: support load `KERNEL_VOLUME_MOUNTS` and `KERNEL_VOLUMES`

--- src/jkclient/schema.py
+++ src/jkclient/schema.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import json
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class CreateKernelRequest(BaseModel):
@@ -10,6 +11,37 @@ class CreateKernelRequest(BaseModel):
     """Kernel spec name (defaults to default kernel spec for server)."""
     env: dict[str, Any] = {}
     """A dictionary of environment variables and values to include in the kernel process - subject to filtering."""
+
+    @field_validator("env")
+    @classmethod
+    def convert_env_value_to_list(cls, v: dict[str, Any]) -> dict[str, str]:
+        if "KERNEL_VOLUME_MOUNTS" in v:
+            kernel_volume_mounts = v["KERNEL_VOLUME_MOUNTS"]
+            if isinstance(kernel_volume_mounts, str):
+                try:
+                    kernel_volume_mounts = json.loads(kernel_volume_mounts)
+                except ValueError:
+                    err_msg = "`KERNEL_VOLUME_MOUNTS` must be a json str"
+                    raise ValueError(err_msg)
+            if not isinstance(kernel_volume_mounts, list):
+                err_msg = "`KERNEL_VOLUME_MOUNTS` must be a list or json list str"
+                raise ValueError(err_msg)
+            v["KERNEL_VOLUME_MOUNTS"] = kernel_volume_mounts
+
+        if "KERNEL_VOLUMES" in v:
+            kernel_volumes = v["KERNEL_VOLUMES"]
+            if isinstance(v["KERNEL_VOLUMES"], str):
+                try:
+                    kernel_volumes = json.loads(kernel_volumes)
+                except ValueError:
+                    err_msg = "`KERNEL_VOLUMES` must be a json str"
+                    raise ValueError(err_msg)
+            if not isinstance(kernel_volumes, list):
+                err_msg = "`KERNEL_VOLUME` must be a list or json list str"
+                raise ValueError(err_msg)
+            v["KERNEL_VOLUMES"] = kernel_volumes
+
+        return v
 
     def model_dump(self):
         return super().model_dump(by_alias=True, exclude_none=True)
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -42,5 +42,43 @@ def test_create_kernel_request_env() -> None:
     assert request.env["KERNEL_NAMESPACE"] == "default"
 
 
+def test_create_kernel_request_env_error() -> None:
+    name = f"kernel-{uuid4().hex}"
+    env = {
+        "KERNEL_USERNAME": "jovyan",
+        "KERNEL_NAMESPACE": "default",
+        "KERNEL_IMAGE": "weekenthralling/kernel-py:0.0.1",
+        "KERNEL_WORKING_DIR": "/mnt/data",
+        "KERNEL_VOLUME_MOUNTS": "foo-0",
+        "KERNEL_VOLUMES": "bar-0",
+        "KERNEL_STARTUP_SCRIPTS_PATH": "/opt/startup",
+    }
+    with pytest.raises(ValueError) as excinfo:
+        CreateKernelRequest(name=name, env=env)
+        assert str(excinfo.value) == "`KERNEL_VOLUME_MOUNTS` must be a json str"
+
+
+def test_create_kernel_request_env_volumns() -> None:
+    import json
+
+    name = f"kernel-{uuid4().hex}"
+    env = {
+        "KERNEL_USERNAME": "jovyan",
+        "KERNEL_NAMESPACE": "default",
+        "KERNEL_IMAGE": "weekenthralling/kernel-py:0.0.1",
+        "KERNEL_WORKING_DIR": "/mnt/data",
+        "KERNEL_VOLUME_MOUNTS": '[{"name":"shared-vol","mount_path":"/mnt/data"},{"name":"ipython-profile-vol","mount_path":"/opt/startup"}]',
+        "KERNEL_VOLUMES": '[{"name":"shared-vol","nfs":{"server":"10.0.0.29","path":"/data/tablegpt-slim-py/data"}},{"name":"ipython-profile-vol","config_map":{"name":"ipython-startup-scripts"}}]',
+        "KERNEL_STARTUP_SCRIPTS_PATH": "/opt/startup",
+    }
+    request = CreateKernelRequest(name=name, env=env)
+    assert isinstance(request.env["KERNEL_VOLUME_MOUNTS"], list)
+    assert request.env["KERNEL_VOLUME_MOUNTS"] == json.loads(
+        env["KERNEL_VOLUME_MOUNTS"]
+    )
+    assert isinstance(request.env["KERNEL_VOLUMES"], list)
+    assert request.env["KERNEL_VOLUMES"] == json.loads(env["KERNEL_VOLUMES"])
+
+
 if __name__ == "__main__":
     pytest.main()

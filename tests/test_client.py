3c6f79f72a18d63729cc984abc1e3055623654d3
Mo Zhou <weekenthralling@gmial.com>
2024-07-30 15:29:45 +0800

    fix: raise forbidden error when create kernels

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 import pytest
 
 from jkclient import CreateKernelRequest, JupyterKernelClient
+from jkclient.schema import KernelCreationForbiddenError
 
 
 @pytest.fixture(scope="module")
@@ -12,140 +13,8 @@ def kernel_client() -> Iterator[JupyterKernelClient]:
     yield client
 
 
-@pytest.mark.skip(reason="Create kernel with kubernetes config")
-def test_create_kernel(kernel_client: JupyterKernelClient) -> None:
-    request = CreateKernelRequest(
-        name="foo-0",
-        env={
-            "KERNEL_ID": str(uuid4()),
-            "KERNEL_USERNAME": "jovyan",
-            "KERNEL_NAMESPACE": "default",
-            "KERNEL_IMAGE": "weekenthralling/kernel-py:133fbe3",
-            "KERNEL_WORKING_DIR": "/mnt/data",
-            "KERNEL_VOLUME_MOUNTS": [
-                {"name": "shared-vol", "mountPath": "/mnt/data"},
-            ],
-            "KERNEL_VOLUMES": [
-                {
-                    "name": "shared-vol",
-                    "nfs": {"server": "10.0.0.29", "path": "/data"},
-                },
-            ],
-            "KERNEL_STARTUP_SCRIPTS_PATH": "/opt/startup",
-            "KERNEL_IDLE_TIMEOUT": "1800",
-        },
-    )
-    response = kernel_client.create(request=request)
-    assert response is not None
-
-
-@pytest.mark.skip(reason="Create kernel with kubernetes config")
-def test_create_kernel_without_name(kernel_client: JupyterKernelClient) -> None:
-    request = CreateKernelRequest(
-        env={
-            "KERNEL_ID": str(uuid4()),
-            "KERNEL_USERNAME": "jovyan",
-            "KERNEL_NAMESPACE": "default",
-            "KERNEL_IMAGE": "weekenthralling/kernel-py:133fbe3",
-            "KERNEL_WORKING_DIR": "/mnt/data",
-            "KERNEL_VOLUME_MOUNTS": [
-                {"name": "shared-vol", "mountPath": "/mnt/data"},
-            ],
-            "KERNEL_VOLUMES": [
-                {
-                    "name": "shared-vol",
-                    "nfs": {"server": "10.0.0.29", "path": "/data"},
-                },
-            ],
-            "KERNEL_STARTUP_SCRIPTS_PATH": "/opt/startup",
-            "KERNEL_IDLE_TIMEOUT": "1800",
-        },
-    )
-    response = kernel_client.create(request=request)
-    assert response is not None
-
-
-@pytest.mark.skip(reason="Create kernel with kubernetes config")
-def test_create_kernel_with_work_dir(kernel_client: JupyterKernelClient) -> None:
-    request = CreateKernelRequest(
-        env={
-            "KERNEL_ID": str(uuid4()),
-            "KERNEL_USERNAME": "tablegpt",
-            "KERNEL_NAMESPACE": "default",
-            "KERNEL_IMAGE": "zjuici/tablegpt-kernel:0.1.1",
-            "KERNEL_WORKING_DIR": "/mnt/data",
-            "KERNEL_VOLUME_MOUNTS": [
-                {"name": "shared-vol", "mountPath": "/mnt/data"},
-                {"name": "ipython-profile-vol", "mountPath": "/opt/startup"},
-                {
-                    "name": "kernel-launch-vol",
-                    "mountPath": "/usr/local/bin/bootstrap-kernel.sh",
-                    "subPath": "bootstrap-kernel.sh",
-                },
-                {
-                    "name": "kernel-launch-vol",
-                    "mountPath": "/usr/local/bin/kernel-launchers/python/scripts/launch_ipykernel.py",
-                    "subPath": "launch_ipykernel.py",
-                },
-            ],
-            "KERNEL_VOLUMES": [
-                {
-                    "name": "shared-vol",
-                    "nfs": {
-                        "server": "10.0.0.29",
-                        "path": "/data/tablegpt-slim-py/data",
-                    },
-                },
-                {
-                    "name": "ipython-profile-vol",
-                    "configMap": {"name": "ipython-startup-scripts"},
-                },
-                {
-                    "name": "kernel-launch-vol",
-                    "configMap": {
-                        "defaultMode": 0o755,
-                        "name": "kernel-launch-scripts",
-                    },
-                },
-            ],
-            "KERNEL_STARTUP_SCRIPTS_PATH": "/opt/startup",
-            "KERNEL_IDLE_TIMEOUT": "1800",
-        },
-    )
-    response = kernel_client.create(request)
-    assert response is not None
-
-
-@pytest.mark.skip(reason="Get kernel with kubernetes config")
-def test_get_kernel(kernel_client: JupyterKernelClient) -> None:
-    kernel = kernel_client.get(name="foo-0", namespace="default")
-    assert kernel is not None
-
-
-@pytest.mark.skip(reason="Get kernel with kubernetes config")
-def test_get_kernel_none(kernel_client: JupyterKernelClient) -> None:
-    with pytest.raises(RuntimeError):
-        kernel_client.get(name="foo-1")
-
-
-@pytest.mark.skip(reason="Delete kernel with kubernetes config")
-def test_delete_kernel(kernel_client: JupyterKernelClient) -> None:
-    kernel_client.delete(name="foo-1", namespace="default")
-
-
-@pytest.mark.skip(reason="Delete kernel with kubernetes config")
-def test_delete_kernel_by_kernel_id(kernel_client: JupyterKernelClient) -> None:
-    kernel_client.delete_by_kernel_id(kerenl_id="968183bb-13ef-4faf-b7d8-30fe8d20e6a3")
-
-
-@pytest.mark.skip(reason="Delete kernel with kubernetes config")
-def test_delete_kernel_none(kernel_client: JupyterKernelClient) -> None:
-    kernel_client.delete_by_kernel_id(kerenl_id=str(uuid4()))
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip(reason="Create kernel with kubernetes config")
-async def test_acreate_kernel(kernel_client: JupyterKernelClient) -> None:
+@pytest.fixture(scope="module")
+def create_kernel_request() -> Iterator[CreateKernelRequest]:
     request = CreateKernelRequest(
         env={
             "KERNEL_ID": str(uuid4()),
@@ -191,8 +60,61 @@ async def test_acreate_kernel(kernel_client: JupyterKernelClient) -> None:
             "KERNEL_IDLE_TIMEOUT": "1800",
         },
     )
-    response = await kernel_client.acreate(request)
+
+    yield request
+
+
+@pytest.mark.skip(reason="Create kernel with kubernetes config")
+def test_create_kernel(
+    kernel_client: JupyterKernelClient, create_kernel_request: CreateKernelRequest
+) -> None:
+    response = kernel_client.create(request=create_kernel_request)
     assert response is not None
+
+
+@pytest.mark.skip(reason="Get kernel with kubernetes config")
+def test_get_kernel(kernel_client: JupyterKernelClient) -> None:
+    kernel = kernel_client.get(name="foo-0", namespace="default")
+    assert kernel is not None
+
+
+@pytest.mark.skip(reason="Get kernel with kubernetes config")
+def test_get_kernel_none(kernel_client: JupyterKernelClient) -> None:
+    with pytest.raises(RuntimeError):
+        kernel_client.get(name="foo-1")
+
+
+@pytest.mark.skip(reason="Delete kernel with kubernetes config")
+def test_delete_kernel(kernel_client: JupyterKernelClient) -> None:
+    kernel_client.delete(name="foo-1", namespace="default")
+
+
+@pytest.mark.skip(reason="Delete kernel with kubernetes config")
+def test_delete_kernel_by_kernel_id(kernel_client: JupyterKernelClient) -> None:
+    kernel_client.delete_by_kernel_id(kerenl_id="968183bb-13ef-4faf-b7d8-30fe8d20e6a3")
+
+
+@pytest.mark.skip(reason="Delete kernel with kubernetes config")
+def test_delete_kernel_none(kernel_client: JupyterKernelClient) -> None:
+    kernel_client.delete_by_kernel_id(kerenl_id=str(uuid4()))
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason="Create kernel with kubernetes config")
+async def test_acreate_kernel(
+    kernel_client: JupyterKernelClient, create_kernel_request: CreateKernelRequest
+) -> None:
+    response = await kernel_client.acreate(create_kernel_request)
+    assert response is not None
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason="Create kernel with kubernetes config and resourcequota limit")
+async def test_acreate_kernel_w_err(
+    kernel_client: JupyterKernelClient, create_kernel_request: CreateKernelRequest
+) -> None:
+    with pytest.raises(KernelCreationForbiddenError):
+        await kernel_client.acreate(create_kernel_request)
 
 
 @pytest.mark.asyncio
